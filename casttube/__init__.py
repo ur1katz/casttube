38478e2ba63bc15051fc1842ab1585e05e34e8e9
Uri Katz <4urikatz@gmail.com>
2018-06-01 10:16:55 +0300

    Initial commit

---
+++ casttube/__init__.py
@@ -0,0 +1,1 @@
+from .YouTubeSession import YouTubeSession
---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='casttube',
+      version='0.1',
+      description='YouTube chromecast api',
+      url='http://github.com/ur1katz/casttube',
+      author='Uri Katz',
+      author_email='4urikatz@gmail.com',
+      license='MIT',
+      packages=['casttube'],
+      zip_safe=False,
+      install_requires=['requests'])
