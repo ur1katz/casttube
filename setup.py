26ce5801e45a3cfd27d25bf2be502adb9fa39be8
Uri Katz <4urikatz@gmail.com>
2018-06-01 13:24:24 +0300

    Long description fro setup.py

--- setup.py
+++ setup.py
@@ -1,8 +1,13 @@
 from setuptools import setup
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 setup(name='casttube',
       version='0.1.0',
       description='YouTube chromecast api',
+      long_description=long_description,
+      long_description_content_type="text/markdown",
       url='http://github.com/ur1katz/casttube',
       author='Uri Katz',
       author_email='4urikatz@gmail.com',
